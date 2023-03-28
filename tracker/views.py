768983a93960eb8d14974a1d5a98d22f5141c0bf
Sergii Machulin <machulinsv@gmail.com>
2023-03-28 20:06:25 +0300

    Add index function for rendering home page

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from tracker.models import Newspaper, Topic
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_publishers = get_user_model().objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+
+    context = {
+        "num_publishers": num_publishers,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+    }
+
+    return render(request, "tracker/index.html", context=context)
