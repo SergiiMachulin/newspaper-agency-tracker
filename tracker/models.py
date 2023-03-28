c33e93853023fb89006c49a76e62579a11b89451
Sergii Machulin <machulinsv@gmail.com>
2023-03-28 18:39:01 +0300

    Add null option to years field of Redactor model

--- tracker/models.py
+++ tracker/models.py
@@ -14,7 +14,7 @@ class Topic(models.Model):
 
 
 class Redactor(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(null=True)
 
     class Meta:
         verbose_name = "publisher"
