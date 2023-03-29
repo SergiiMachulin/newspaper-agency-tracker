6c98f64148233fe9e13b88cda93ae736d992f9c4
Sergii Machulin <machulinsv@gmail.com>
2023-03-29 11:38:40 +0300

    Setup debug toolbar

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("tracker.urls", namespace="tracker"))
+    path("", include("tracker.urls", namespace="tracker")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
